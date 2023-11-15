c1ab30bf46318b031df304787fe2588b6aedf4d1
Mohammed El Mouden <mohamed55mouden@gmail.com>
2023-11-15 11:19:05 +0100

    0auth set up

--- client/my-react-app/src/pages/Users.jsx
+++ client/my-react-app/src/pages/Users.jsx
@@ -11,9 +11,13 @@ import { getUsers } from "../redux/actions/AuthAction";
 const Users = ({ navVisible }) => {
   const [searchValue, setSearchValue] = useState("");
   const date = new Date();
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const Todaysdate = date.toLocaleDateString('en-us', options)
-
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const Todaysdate = date.toLocaleDateString("en-us", options);
 
   const [showAddUserForm, setShowAddUserForm] = useState(false);
   const [showUpdateUserForm, setShowUpdateUserForm] = useState(false);
@@ -35,22 +39,24 @@ const Users = ({ navVisible }) => {
   return (
     <>
       <div className={navVisible ? "page page-with-navbar" : "page"}>
-      <div className="w-full flex justify-around">
-      <label className="label">
-        <span className="text-xs sm:text-sm md:text-md xl:text-xl 2xl:text-xl text-primary font-roboto">{Todaysdate}</span>
-      </label>
+        <div className="w-full flex justify-around">
+          <label className="label">
+            <span className="text-xs sm:text-sm md:text-md xl:text-xl 2xl:text-xl text-primary font-roboto">
+              {Todaysdate}
+            </span>
+          </label>
 
-        <form className="flex flex-row-reverse items-center form-control">
-          <input
-            className="bg-transparent outline-none input-xs sm:input-sm md:input-sm lg:input-md xl:input-md 2xl:input-lg "
-            placeholder="  Search By Username"
-            type="text"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-          />
-          <AiOutlineSearch className="" />
-        </form>
-      </div>
+          <form className="flex flex-row-reverse items-center form-control">
+            <input
+              className="bg-transparent outline-none input-xs sm:input-sm md:input-sm lg:input-md xl:input-md 2xl:input-lg "
+              placeholder="  Search By Username"
+              type="text"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <AiOutlineSearch className="" />
+          </form>
+        </div>
         <div className="w-5/6 h-3/4 bg-secondary rounded-2xl px-2 py-10 flex flex-col items-center justify-around">
           <div className="flex justify-around w-full">
             <button
@@ -61,16 +67,20 @@ const Users = ({ navVisible }) => {
             >
               Add User
             </button>
-            <h1 className="font-roboto text-center z-50 text-md 2xl:text-3xl xl:text-2xl lg:text-xl md:text-xl sm:text-lg">Users:</h1>
+            <h1 className="font-roboto text-center z-50 text-md 2xl:text-3xl xl:text-2xl lg:text-xl md:text-xl sm:text-lg">
+              Users:
+            </h1>
             <label className="flex flex-col">
-              <h1 className="font-roboto text-center z-50 text-md 2xl:text-2xl xl:text-xl lg:text-xl md:text-xl sm:text-lg">Total Users :</h1>
+              <h1 className="font-roboto text-center z-50 text-md 2xl:text-2xl xl:text-xl lg:text-xl md:text-xl sm:text-lg">
+                Total Users :
+              </h1>
               <span className="self-end font-roboto text-center 2xl:text-2xl xl:text-xl lg:text-lg md:text-md sm:text-sm text-accent font-semibold">
                 {testingInfo ? testingInfo.length : 0}
               </span>
             </label>
             <motion.div
               whileTap={{ scale: 0.98 }}
-              dragElastic = {{top: 0, bottom: 0.3}}
+              dragElastic={{ top: 0, bottom: 0.3 }}
               className="bg-primary absolute z-50 w-fit p-3 justify-center items rounded-xl flex"
               drag
             >
